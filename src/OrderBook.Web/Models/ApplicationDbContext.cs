6a6e9ed01d4708d76bfece0213067661ec14e07f
adrskw <48623654+adrskw@users.noreply.github.com>
2020-04-24 20:45:34 +0200

    Remove Employee related objects and reset database structure

--- src/OrderBook.Web/Models/ApplicationDbContext.cs
+++ src/OrderBook.Web/Models/ApplicationDbContext.cs
@@ -8,8 +8,6 @@ namespace OrderBook.Web.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Employee> Employees { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
