00d2aa0e701f906a69b148d1f2c10cb8b01bc193
adrskw <48623654+adrskw@users.noreply.github.com>
2020-04-25 00:46:30 +0200

    Rename login page and set it to the main page

--- src/OrderBook.Web/Controllers/HomeController.cs
+++ src/OrderBook.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ namespace OrderBook.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
