7ede4f33f6c29b598c964c55c0af81948cc10208
adrskw <48623654+adrskw@users.noreply.github.com>
2020-07-19 17:06:11 +0200

    Implement login functionality

--- src/OrderBook.Web/Controllers/HomeController.cs
+++ src/OrderBook.Web/Controllers/HomeController.cs
@@ -1,15 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OrderBook.Web.Models;
+using OrderBook.Web.ViewModels;
 
 namespace OrderBook.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public HomeController(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
         [Route("")]
         [Route("Home")]
         [Route("Home/Index")]
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(userName: model.Login,
+                                                                    password: model.Password,
+                                                                    isPersistent: model.RememberMe,
+                                                                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "dashboard");
+                }
+
+                ModelState.AddModelError("", "Logowanie nie powiodło się");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
     }
 }
---
+++ src/OrderBook.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderBook.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Login jest wymagany")]
+        [Display(Name = "Login", Prompt = "login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło", Prompt = "hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
