8ce102dd8a8154e2f82492813fd5d34378c6af0a
adrskw <48623654+adrskw@users.noreply.github.com>
2020-09-03 22:36:28 +0200

    Show list of positions (roles)

---
+++ src/OrderBook.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.CodeAnalysis.Differencing;
+using OrderBook.Web.Models;
+using OrderBook.Web.ViewModels;
+
+namespace OrderBook.Web.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Position Actions
+
+        public IActionResult ListPositions()
+        {
+            var positions = roleManager.Roles.OrderBy(position => position.Name);
+
+            return View(positions);
+        }
+
+        #endregion Position Actions
+    }
+}
