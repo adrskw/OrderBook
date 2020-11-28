a92d18ee08c34ab35699ad339816b443f8bc3786
adrskw <48623654+adrskw@users.noreply.github.com>
2020-11-28 10:56:57 +0100

    Add interfaces to track and delete entities

---
+++ src/OrderBook.Web/Models/IDeletable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderBook.Web.Models
+{
+    public interface IDeletable
+    {
+        bool IsDeleted { get; set; }
+        DateTime? DeletedAt { get; set; }
+        ApplicationUser DeletedBy { get; set; }
+    }
+}
---
+++ src/OrderBook.Web/Models/ITrackable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderBook.Web.Models
+{
+    public interface ITrackable
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public ApplicationUser CreatedBy { get; set; }
+        public ApplicationUser UpdatedBy { get; set; }
+    }
+}
