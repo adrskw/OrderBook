b3aa178d996c208b8aae131e61c1cf7417855afb
adrskw <48623654+adrskw@users.noreply.github.com>
2021-03-01 00:53:34 +0100

    Create repository for Product Categories

---
+++ src/OrderBook.Web/Repositories/IProductCategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderBook.Web.Models;
+
+namespace OrderBook.Web.Repositories
+{
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetAllProductCategories();
+
+        ProductCategory GetById(int id);
+
+        void Add(ProductCategory productCategory);
+
+        void Update(ProductCategory productCategory);
+
+        void Delete(int id);
+
+        void Save();
+    }
+}
---
+++ src/OrderBook.Web/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using OrderBook.Web.Models;
+using OrderBook.Web.Utilities;
+
+namespace OrderBook.Web.Repositories
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductCategoryRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<ProductCategory> GetAllProductCategories()
+        {
+            return context.ProductCategories.ToList();
+        }
+
+        public ProductCategory GetById(int id)
+        {
+            return context.ProductCategories.Find(id);
+        }
+
+        public void Add(ProductCategory productCategory)
+        {
+            context.ProductCategories.Add(productCategory);
+        }
+
+        public void Update(ProductCategory productCategory)
+        {
+            context.Entry(productCategory).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var productCategory = context.ProductCategories.Find(id);
+
+            context.ProductCategories.Remove(productCategory);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
